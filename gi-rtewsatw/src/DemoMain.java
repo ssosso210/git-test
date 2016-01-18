a2beed51a305aa6e56cd6dbc1971c9da5d71145c
momohey <momohey@naver.com>
2016-01-18 17:04:33 +0900

    add

--- gi-rtewsatw/src/DemoMain.java
+++ gi-rtewsatw/src/DemoMain.java
@@ -4,6 +4,7 @@ public class DemoMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("DemoMain test");
+		System.out.println("test add");
 
 	}
 
